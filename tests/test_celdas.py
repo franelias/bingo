0cba5ab9692e1f756643f64e8858e09f7d66cab2
franelias <fielias81@gmail.com>
2020-04-16 15:33:40 -0300

    Comentadas funciones sin comentar

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,5 +1,5 @@
 from src.bingo import carton
-
+#Test que revisa si la cantidad de celda ocupadas es 15
 def test_contar_celdas_ocupadas():
     mi_carton = carton()
     contador = 0
@@ -8,7 +8,7 @@ def test_contar_celdas_ocupadas():
             contador = contador + celda
 
     assert contador == 15
-
+#Test que revisa si la cantidad de celdas ocupadas no es mayor a 15
 def test_menos_de_15_celdas_ocupadas():
     mi_carton = carton();
     contador = 0
@@ -17,7 +17,7 @@ def test_menos_de_15_celdas_ocupadas():
             contador = contador + celda
 
     assert contador <= 15
-
+#Test que revisa si la cantidad de celdas ocupadas no es menor a 15
 def test_mas_de_15_celdas_ocupadas():
     mi_carton = carton();
     contador = 0
@@ -26,7 +26,7 @@ def test_mas_de_15_celdas_ocupadas():
             contador = contador + celda
 
     assert contador >= 15
-
+#Test que revisa si hay al menos una celda ocupada en cada columna del carton
 def test_revisar_columnas_ocupadas():
     mi_carton = carton()
     ban = 0
