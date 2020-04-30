4d06660070fd69c08da5ea0c4f6eafada34e8820
franelias <fielias81@gmail.com>
2020-04-30 13:45:50 -0300

    Reestructurado el codigo cambiando la manera en que los tests validan los cartones

--- src/bingo.py
+++ src/bingo.py
@@ -1,8 +1,35 @@
 
 def carton():
     carton = (
-        (1,0,0,1,1,0,1,0,1),
-        (0,1,1,1,0,1,1,1,1),
-        (0,1,0,0,1,0,0,1,0)
+        (5,10,0,0,44,0,62,70,0),
+        (0,16,0,37,47,0,0,76,81),
+        (7,0,21,39,0,58,0,0,89)
     )
     return carton
+
+def validar_quince_numeros(carton):
+    celdas_vacias = 0
+    for fila in carton:
+        for celda in fila:
+            if celda == 0:
+                celdas_vacias = celdas_vacias + 1
+
+    return celdas_vacias == 12
+
+def validar_menos_quince_numeros(carton):
+    celdas_vacias = 0
+    for fila in carton:
+        for celda in fila:
+            if celda == 0:
+                celdas_vacias = celdas_vacias + 1
+
+    return celdas_vacias <= 12
+
+def validar_mas_quince_numeros(carton):
+    celdas_vacias = 0
+    for fila in carton:
+        for celda in fila:
+            if celda == 0:
+                celdas_vacias = celdas_vacias + 1
+
+    return celdas_vacias >= 12
--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,44 +1,31 @@
 from src.bingo import carton
+from src.bingo import validar_quince_numeros
+from src.bingo import validar_menos_quince_numeros
+from src.bingo import validar_mas_quince_numeros
 
 #Test que revisa si la cantidad de celda ocupadas es 15
 def test_contar_celdas_ocupadas():
     mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-
-    assert contador == 15
+    assert validar_quince_numeros(mi_carton)
 
 #Test que revisa si la cantidad de celdas ocupadas no es mayor a 15
 def test_menos_de_15_celdas_ocupadas():
-    mi_carton = carton();
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-
-    assert contador <= 15
+    mi_carton = carton()
+    assert validar_menos_quince_numeros(mi_carton)
 
 #Test que revisa si la cantidad de celdas ocupadas no es menor a 15
 def test_mas_de_15_celdas_ocupadas():
-    mi_carton = carton();
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-
-    assert contador >= 15
+    mi_carton = carton()
+    assert validar_mas_quince_numeros(mi_carton)
 
 #Test que revisa si hay al menos una celda ocupada en cada columna del carton
 def test_revisar_columnas_ocupadas():
     mi_carton = carton()
-    ban = 0
     for x in range(9):
         if mi_carton[0][x] == 0 and mi_carton[1][x] == 0 and mi_carton[2][x] == 0:
-            ban = 1
+            assert False
 
-    assert ban == 0
+    assert True
 
 #Funcion que cuenta las celdas ocupadas por fila
 def contar_celdas_por_fila(carton, fila):
