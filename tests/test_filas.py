fd072d6fb67aa1dcfdb477cac72ddddf1350aa76
franelias <fielias81@gmail.com>
2020-06-06 16:40:48 -0300

    Comentados tests

--- tests/test_filas.py
+++ tests/test_filas.py
@@ -4,19 +4,22 @@ from src.bingo import filas_2_celdas_consecutivas_vacias
 from src.bingo import revisar_filas_ocupadas
 from src.bingo import filas_2_celdas_consecutivas_ocupadas
 
+#Revisa que por fila haya solo 5 celdas ocupadas
 def test_fila_5_celdas_ocupadas():
     assert fila_5_celdas_ocupadas(carton_valido)
     assert not fila_5_celdas_ocupadas(carton_invalido4)
 
+#Revisa que no haya celdas con más de 2 celdas consecutivas vacías
 def test_filas_celdas_consecutivas_vacias():
     assert filas_2_celdas_consecutivas_vacias(carton_valido)
     assert not filas_2_celdas_consecutivas_vacias(carton_invalido4)
 
-#Test que valida que hay por lo menos una celda ocupada por fila
+#Revisa que haya por lo menos una celda ocupada por fila
 def test_revisar_filas_ocupadas():
     assert revisar_filas_ocupadas(carton_valido)
     assert not revisar_filas_ocupadas(carton_invalido4)
 
+#Revisa que no haya celdas con más de 2 celdas consecutivas ocupadas
 def test_filas_2_celdas_consecutivas_ocupadas():
     assert filas_2_celdas_consecutivas_ocupadas(carton_valido)
     assert not filas_2_celdas_consecutivas_ocupadas(carton_invalido4)
