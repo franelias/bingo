d24f982cb54c75d0d9b59aaab00224b50d407a9f
franelias <fielias81@gmail.com>
2020-07-01 14:13:23 -0300

    Cambiados nombres de las funciones unidas

--- tests/test_filas.py
+++ tests/test_filas.py
@@ -1,8 +1,7 @@
 from tests.cartones import *
 from src.bingo import fila_5_celdas_ocupadas
-from src.bingo import filas_2_celdas_consecutivas_vacias
 from src.bingo import revisar_filas_ocupadas
-from src.bingo import filas_2_celdas_consecutivas_ocupadas
+from src.bingo import filas_2_celdas_consecutivas_vacias_o_ocupadas
 
 #Revisa que por fila haya solo 5 celdas ocupadas
 def test_fila_5_celdas_ocupadas():
@@ -11,8 +10,8 @@ def test_fila_5_celdas_ocupadas():
 
 #Revisa que no haya celdas con más de 2 celdas consecutivas vacías
 def test_filas_celdas_consecutivas_vacias():
-    assert filas_2_celdas_consecutivas_vacias(carton_valido)
-    assert not filas_2_celdas_consecutivas_vacias(carton_invalido4)
+    assert filas_2_celdas_consecutivas_vacias_o_ocupadas(carton_valido)
+    assert not filas_2_celdas_consecutivas_vacias_o_ocupadas(carton_invalido4)
 
 #Revisa que haya por lo menos una celda ocupada por fila
 def test_revisar_filas_ocupadas():
@@ -21,5 +20,5 @@ def test_revisar_filas_ocupadas():
 
 #Revisa que no haya celdas con más de 2 celdas consecutivas ocupadas
 def test_filas_2_celdas_consecutivas_ocupadas():
-    assert filas_2_celdas_consecutivas_ocupadas(carton_valido)
-    assert not filas_2_celdas_consecutivas_ocupadas(carton_invalido4)
+    assert filas_2_celdas_consecutivas_vacias_o_ocupadas(carton_valido)
+    assert not filas_2_celdas_consecutivas_vacias_o_ocupadas(carton_invalido4)
